0755d3b3589f8df85859c29632b4c4916ff6043d
LUMO66666 <liuwl20t@gmail.com>
2025-11-05 02:57:50 +0000

    add 2 node

--- examples/multiturn_demo/run_main_searchr1.sh
+++ examples/multiturn_demo/run_main_searchr1.sh
@@ -12,7 +12,7 @@ MEGATRON_PATH=/opt/Megatron-LM
 export PYTHONPATH=${REPO_PATH}:${MEGATRON_PATH}:$PYTHONPATH
 
 if [ -z "$1" ]; then
-    CONFIG_NAME="qwen2.5-1.5b-tool"
+    CONFIG_NAME="qwen2.5-1.5b-tool-2node"
 else
     CONFIG_NAME=$1
 fi
