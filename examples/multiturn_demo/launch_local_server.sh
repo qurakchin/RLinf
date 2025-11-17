e232724d40efc37b6744dfa599b71f8c6d15cfd0
LUMO666 <liuwl20t@gmail.com>
2025-11-17 06:51:26 +0000

    fix_stopstring

--- examples/multiturn_demo/launch_local_server.sh
+++ examples/multiturn_demo/launch_local_server.sh
@@ -5,13 +5,13 @@
 
 set -ex
 
-WIKI2018_WORK_DIR=/inspire/hdd/global_user/liboxun-p-liboxun/data/data/ASearcher-Local-Knowledge
+WIKI2018_WORK_DIR=/mnt/public/xzxuan/data/Asearch
 
 index_file=$WIKI2018_WORK_DIR/e5.index/e5_Flat.index
 corpus_file=$WIKI2018_WORK_DIR/wiki_corpus.jsonl
 pages_file=$WIKI2018_WORK_DIR/wiki_webpages.jsonl
 retriever_name=e5
-retriever_path=/inspire/hdd/global_user/liboxun-p-liboxun/data/e5-base-v2
+retriever_path=/mnt/public/xzxuan/model/e5
 
 python3  ./local_retrieval_server.py --index_path $index_file \
                                             --corpus_path $corpus_file \
@@ -20,4 +20,4 @@ python3  ./local_retrieval_server.py --index_path $index_file \
                                             --retriever_name $retriever_name \
                                             --retriever_model $retriever_path \
                                             --faiss_gpu --port 8000 \
-                                            --save-address-to /inspire/hdd/global_user/liboxun-p-liboxun/data/data/ASearcher-Local-Knowledge
+                                            --save-address-to /mnt/public/liuweilin/local_server
