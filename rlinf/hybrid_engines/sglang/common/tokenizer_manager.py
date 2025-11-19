75e661a618ebcc43c854ecbdab6d77054ada4531
qurakchin <czzcy3832515@hotmail.com>
2025-11-19 18:14:56 +0800

    fix: fix use correct output_ids in sglang 0.4
    Signed-off-by: qurakchin <czzcy3832515@hotmail.com>

--- rlinf/hybrid_engines/sglang/common/tokenizer_manager.py
+++ rlinf/hybrid_engines/sglang/common/tokenizer_manager.py
@@ -125,7 +125,7 @@ class TokenizerManager(_TokenizerManager):
         )
 
         # for sglang 0.5.0 and later, we use the original _handle_batch_output
-        if not self.patch_handle_batch_output:
+        if not self.patch_return_output_ids:
             return super()._handle_batch_output(recv_obj)
 
         for i, rid in enumerate(recv_obj.rids):
