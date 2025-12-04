26e5f89c71ba157189605adc5b3ac3b3fc649281
LUMO666 <liuwl20t@gmail.com>
2025-12-04 03:02:51 +0000

    searchr1 on main

---
+++ examples/searchr1/launch_local_server_local.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# source /root/miniconda3/etc/profile.d/conda.sh
+# conda activate
+
+set -ex
+
+WIKI2018_WORK_DIR=/mnt/public/xzxuan/data/Asearch
+
+index_file=$WIKI2018_WORK_DIR/e5.index/e5_Flat.index
+corpus_file=$WIKI2018_WORK_DIR/wiki_corpus.jsonl
+pages_file=$WIKI2018_WORK_DIR/wiki_webpages.jsonl
+retriever_name=e5
+retriever_path=/mnt/public/xzxuan/model/e5
+
+python3  ./local_retrieval_server.py --index_path $index_file \
+                                            --corpus_path $corpus_file \
+                                            --pages_path $pages_file \
+                                            --topk 3 \
+                                            --retriever_name $retriever_name \
+                                            --retriever_model $retriever_path \
+                                            --faiss_gpu --port 8000 \
+                                            --save-address-to /mnt/public/liuweilin/local_server
