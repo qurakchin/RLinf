2999b8d7e34a3acdab81783eaf8db85d2b1878a3
qurakchin <czzcy3832515@hotmail.com>
2026-03-10 16:53:01 +0800

    fix: fix repo pathh in scripts of agents
    Signed-off-by: qurakchin <czzcy3832515@hotmail.com>

--- examples/agent/mas-search/run_main_mas_search.sh
+++ examples/agent/mas-search/run_main_mas_search.sh
@@ -7,7 +7,7 @@ export TOKENIZERS_PARALLELISM=false
 export RAY_DEDUP_LOGS=0
 
 CONFIG_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-REPO_PATH=$(dirname $(dirname "$CONFIG_PATH"))
+REPO_PATH=$(dirname $(dirname $(dirname "$CONFIG_PATH")))
 MEGATRON_PATH=/opt/Megatron-LM
 export PYTHONPATH=${REPO_PATH}:${MEGATRON_PATH}:${PYTHONPATH}
 
